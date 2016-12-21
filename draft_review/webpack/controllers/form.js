a02794ce892823c966cd96d4d703c73c5df340ab
Brian Gracie <bgracie@gmail.com>
2016-12-20 20:14:00 -0500

    Add reviewable select

--- draft_review/webpack/controllers/form.js
+++ draft_review/webpack/controllers/form.js
@@ -10,6 +10,18 @@ const Form = ({model, dispatch}) => {
         label="Restaurant name:"
         dispatch={dispatch}
       />
+      <ReviewableSelect
+        attrName="price_key"
+        model={model}
+        label="Price level"
+        dispatch={dispatch}
+      >
+        {
+          _.map(model.app.prices, (label, value) => {
+            return(<option value={value} key={value}>{ label }</option>);
+          })
+        }
+      </ReviewableSelect>
     </form>
   );
 }
@@ -19,9 +31,33 @@ const basePath = ["ui", "formData"];
 const ReviewableFieldset = ({model, attrName, handleCurrent, handleRemoved, dispatch}) => {
   <div>
     {
-      
+
     }
   </div>
+}
+
+const ReviewableSelect = ({model, attrName, label, dispatch, children}) => {
+  return(
+    <div>
+      <select value={ _.get(model, basePath.concat("draft", attrName)) }
+        onChange={_.partial(changeInputValue, dispatch, attrName)}
+      >
+        { children }
+      </select>
+      <div style={{color: "orange"}}>
+        {
+          `Was: ${labelValue(attrName, _.get(model, basePath.concat("published", attrName)), model)}`
+        }
+      </div>
+    </div>
+  )
+}
+
+const labelValue = (attrName, value, model) => {
+  switch(attrName) {
+  case "price_key":
+    return model.app.prices[value];
+  }
 }
 
 const ReviewableInput = ({model, attrName, inputParams, label, dispatch}) => {
@@ -34,7 +70,7 @@ const ReviewableInput = ({model, attrName, inputParams, label, dispatch}) => {
           _.assign(
             {
               value: _.get(model, basePath.concat("draft", attrName)),
-              onChange: _.partial(changeInputValue, dispatch, "name")
+              onChange: _.partial(changeInputValue, dispatch, attrName)
             },
             inputParams
           )
--- draft_review/webpack/initial_sample_data.js
+++ draft_review/webpack/initial_sample_data.js
@@ -5,19 +5,19 @@ const initialSampleData = {
         name: "Au Pied de Cochon",
         price_key: 3,
         has_takeout: false,
-        menu_item_links: {
-          3: { preparation_method: "Grilled" },
-          2: { preparation_method: "Barbecued" }
-        }
+        menu_item_links: [
+          { id: 3, preparation_method: "Grilled" },
+          { id: 2, preparation_method: "Barbecued" }
+        ]
       },
       published: {
         name: "Au Pied Cochon typo",
         price_key: 1,
         has_takeout: true,
-        menu_item_links: {
-          3: { preparation_method: "Braised" },
-          5: { preparation_method: "Seared" }
-        }
+        menu_item_links: [
+          { id: 3, preparation_method: "Braised" },
+          { id: 5, preparation_method: "Seared" }
+        ]
       }
     }
   },
@@ -28,6 +28,11 @@ const initialSampleData = {
       3: { id: 3, name: "Pork" },
       4: { id: 4, name: "Carrots" },
       5: { id: 5, name: "Potatos"}
+    },
+    prices: {
+      1: "low",
+      2: "medium",
+      3: "high"
     }
   }
 };
